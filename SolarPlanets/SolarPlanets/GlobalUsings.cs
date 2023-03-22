0d2a7d0e9116b963686538c52b8f031c4b2875d7
risto <ristokoort@yahoo.com>
2023-03-22 16:39:24 +0200

    --------------------

--- SolarPlanets/SolarPlanets/GlobalUsings.cs
+++ SolarPlanets/SolarPlanets/GlobalUsings.cs
@@ -1,1 +1,7 @@
 ﻿global using SolarPlanets.Views;
+
+global using Microsoft.Maui;
+global using Microsoft.Maui.Controls;
+global using Microsoft.Maui.Hosting;
+global using Microsoft.Maui.Graphics;
+global using Microsoft.Maui.LifecycleEvents;
--- SolarPlanets/SolarPlanets/Views/PlanetsPage.xaml.cs
+++ SolarPlanets/SolarPlanets/Views/PlanetsPage.xaml.cs
@@ -2,8 +2,21 @@ namespace SolarPlanets.Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	private const uint AnimationDuration = 800u;
 	public PlanetsPage()
 	{
 		InitializeComponent();
 	}
+
+	async void GridArea_Tapped(System.Object sender,System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+	private async Task CloseMenu()
+	{
+		//close menu and show again main content
+		_ = MainContentGrid.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+		await  MainContentGrid.TranslateTo(0,0, AnimationDuration, Easing.CubicIn);
+    }
 }
