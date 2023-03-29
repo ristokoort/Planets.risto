8f6f28b508cb1d9ca00aa73cfe1122393dbdda60
ristokoort@yahoo.com <>
2023-03-29 15:52:32 +0300

    .................

--- SolarPlanets/SolarPlanets/Models/Planet.cs
+++ SolarPlanets/SolarPlanets/Models/Planet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Models
+namespace SolarPlanets.Models
 {
     public  class Planet
     {
@@ -13,6 +13,27 @@ namespace Models
         public string Description { get; set; }   
         public Microsoft.Maui.Graphics.Color AccentColorStart { get; set; }
         public Microsoft.Maui.Graphics.Color AccentColorEnd { get; set; }
-        public List<string> Images { get; set; }    
+        public List<string> Images { get; set; }
+
+
+        //Background
+        public Brush Background
+        {
+            get
+            {
+                var gradientStops = new GradientStopCollection();
+                gradientStops.Add(new GradientStop(AccentColorStart, 0.0f));
+                gradientStops.Add(new GradientStop(AccentColorEnd, 1.0f));
+
+                var bgBrush = new LinearGradientBrush(gradientStops,
+                    new Point(0.0, 0.0),
+                    new Point(1.0, 1.0));
+
+                return bgBrush;
+            }
+        }
     }
+
+    
+    
 }
--- SolarPlanets/SolarPlanets/Services/PlanetsService.cs
+++ SolarPlanets/SolarPlanets/Services/PlanetsService.cs
@@ -1,4 +1,4 @@
-﻿using Models;
+﻿using SolarPlanets.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
